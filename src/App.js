c568a7b8c323fc188c6d1fd6fa28855ba6ec9a38
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-07-16 21:10:28 +0530

    Add Home Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import Home from './Components/Home'
 
 function App() {
-	return <h1 className="text-4xl font-bold text-blue-500">Hello World!</h1>
+	return (
+		<>
+			<Home />
+		</>
+	)
 }
 
 export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+	return (
+		<div
+			className="relative h-screen w-screen flex flex-col justify-between"
+			style={{
+				backgroundImage:
+					'url(https://images.unsplash.com/photo-1589308454676-06c69786e24c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1575&q=80)',
+				backgroundSize: 'cover',
+				backgroundPosition: 'center'
+			}}></div>
+	)
+}
+
+export default Home
