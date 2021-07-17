787fd4e3f527f3baf4a977b95c802dfe7b0fc02f
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-07-17 14:24:15 +0530

    Add About Component

---
+++ src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Description from './MiniComponents/About/Description'
+import Team from './MiniComponents/About/Team'
+
+const About = () => {
+	return (
+		<div className="w-screen h-screen bg-primary flex flex-col lg:flex-row-reverse justify-evenly items-center">
+			<Description />
+			<Team />
+		</div>
+	)
+}
+
+export default About
