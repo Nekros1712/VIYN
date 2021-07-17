2e6198d60e790d3453d786d63cdb8b3784c1e914
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-07-17 14:22:23 +0530

    Add custom colors and width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#121212',
+				secondary: '#333333'
+			},
+			width: {
+				45: '45%',
+				85: '85%'
+			}
+		}
 	},
 	variants: {
 		extend: {}
