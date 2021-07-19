7a2512a8cf773bd839731dcc3a1cd949adf325a9
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-07-19 17:24:29 +0530

    Complete Contact Section

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ScooterGirl from '../Assets/ScooterGirl.svg'
+import ContactUs from './MiniComponents/Contact/ContactUs'
+import Feedback from './MiniComponents/Contact/Feedback'
+import Footer from './MiniComponents/Contact/Footer'
+
+const Contact = () => {
+	return (
+		<div className="w-screen h-screen bg-primary">
+			<div
+				className="w-full h-3/4 md:w-full md:h-4/5 md:flex md:justify-between md:items-center bg-primary rounded-lg"
+				style={{
+					backgroundImage: `url(${ScooterGirl})`,
+					backgroundSize: 'auto auto',
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center'
+				}}>
+				<ContactUs />
+				<Feedback />
+			</div>
+			<Footer />
+		</div>
+	)
+}
+
+export default Contact
