15f443d1a71e24013918c5f8ac701aa827e52db6
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-08-08 15:04:44 +0530

    Fix CSS Warnings

--- src/Assets/DownArrow.js
+++ src/Assets/DownArrow.js
@@ -12,14 +12,14 @@ const DownArrow = () => {
 			<path
 				d="M17.2223 32.7778C25.8134 32.7778 32.7779 25.8133 32.7779 17.2222C32.7779 8.63115 25.8134 1.66669 17.2223 1.66669C8.63121 1.66669 1.66675 8.63115 1.66675 17.2222C1.66675 25.8133 8.63121 32.7778 17.2223 32.7778Z"
 				stroke="white"
-				stroke-width="2"
+				strokeWidth="2"
 			/>
 			<path
 				d="M11 14.8889L17.2222 21.1111L23.4444 14.8889"
 				stroke="white"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	)
