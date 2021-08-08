4c487d46edb363de8887f77ad7ebd182ef080b70
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-08-08 15:05:31 +0530

    Create Mobile App Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import About from './Components/About'
 import Contact from './Components/Contact'
 import Home from './Components/Home'
+import MobileApp from './Components/MobileApp'
 
 function App() {
 	return (
 		<>
 			<Home />
 			<About />
+			<MobileApp />
 			<Contact />
 		</>
 	)
