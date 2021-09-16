b9b54941813b10afce2c3eda4e4f2f06881e5a01
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-09-16 20:46:49 +0530

    Add image to cloudinary

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({ aboutRef, mobileRef, contactRef }) => {
 			className="relative h-screen w-screen flex flex-col justify-between"
 			style={{
 				backgroundImage:
-					'url(https://images.unsplash.com/photo-1589308454676-06c69786e24c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1575&q=80)',
+					'url(https://res.cloudinary.com/dg7anvkkr/image/upload/v1631805074/viyn/maxim-mushnikov-3zYonE2hNrA-unsplash_2_ufejk0.jpg)',
 				backgroundSize: 'cover',
 				backgroundPosition: 'center',
 				scrollSnapAlign: 'center'
